e0b0910b95152a66e08d5ff9eea399d7f0d16070
Michael Neuder <michael.neuder@gmail.com>
2020-11-19 10:16:13 -0500

    Supporting invalid variable error handling

--- AutoDiffer/core/tests/src/test_ALL.cpp
+++ AutoDiffer/core/tests/src/test_ALL.cpp
@@ -544,3 +544,30 @@ TEST(autodiffer_test_fails_run_invalid, double){
     ASSERT_EQ(res.first.code, ReturnCode::parse_error);
     ASSERT_EQ(res.first.message, "Binary operation requires LHS and RHS");
 }
+
+TEST(autodiffer_test_fails_wrong_var_name, double){
+    AutoDiffer<double> ad;
+    ad.SetSeed("x", /*value=*/2., /*dval=*/1.);
+
+    std::pair<Status, double> res = ad.Derive("(y^2)");
+    EXPECT_EQ(res.first.code, ReturnCode::parse_error);
+    EXPECT_EQ(res.first.message, "Key not found: y");
+}
+
+TEST(autodiffer_test_fails_wrong_var_name2, double){
+    AutoDiffer<double> ad;
+    ad.SetSeed("x", /*value=*/2., /*dval=*/1.);
+
+    std::pair<Status, double> res = ad.Derive("(2^z)");
+    EXPECT_EQ(res.first.code, ReturnCode::parse_error);
+    EXPECT_EQ(res.first.message, "Key not found: z");
+}
+
+TEST(autodiffer_test_fails_wrong_var_name3, double){
+    AutoDiffer<double> ad;
+    ad.SetSeed("x", /*value=*/2., /*dval=*/1.);
+
+    std::pair<Status, double> res = ad.Derive("((sin(4*(x+2)))^z)");
+    EXPECT_EQ(res.first.code, ReturnCode::parse_error);
+    EXPECT_EQ(res.first.message, "Key not found: z");
+}
